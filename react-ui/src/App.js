d2ef46917052b4d7083039a882d98c5956d48154
David Espinoza <despinoza1027@gmail.com>
2017-06-13 19:45:21 -0500

    search results console loggable

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
-import Navigation from './Navigation.js'
+import $ from 'jquery';
+// import Navigation from './Navigation.js'
 
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  componentDidMount() {
+    // this.makeAjaxCall();
+  }
+
+  searchQueryText(evt) {
+    console.log(evt.target.value)
+    this.setState({
+      searchTerm: evt.target.value
+    })
+  }
+
+  makeAjaxCall() {
+    $.ajax({
+      url: 'https://api.github.com/search/repositories?q=' + this.state.searchTerm + '&page=' + '1'
+    })
+    .done((data) => {
+      console.log(data);
+    });
+  };
+
+
   render() {
     return (
       <div className="App">
-        <Navigation />
+        <nav>
+          <p className='search'>SEARCH: </p>
+          <input id='search-query' placeholder="github repository name" onKeyUp={(evt) => this.searchQueryText(evt)}/>
+          <button className='search-button' onClick={() => this.makeAjaxCall()}>SEARCH</button>
+          <p id="previous-button">previous</p>
+          <p id="next-button">next</p>
+          <p className="search-results"></p>
+          <p className="total-results"></p>
+          <p className="total-repos"></p>
+        </nav>
+
+        <ul id='repos'>
+
+        </ul>
       </div>
     );
   }
