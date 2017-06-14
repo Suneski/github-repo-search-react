633434708cec49e3d820ac34f308ee60723abb10
David Espinoza <despinoza1027@gmail.com>
2017-06-13 19:46:46 -0500

    prepared initial Store.js

--- react-ui/src/Navigation.js
+++ react-ui/src/Navigation.js
@@ -4,14 +4,14 @@ class Navigation extends Component {
   render() {
     return (
       <nav>
-      <p className='search'>SEARCH: </p>
-      <input id='search-query' placeholder="github repository name"/>
-      <p id="previous-button">previous</p>
-      <p id="next-button">next</p>
-      <p className="search-results"></p>
-      <p className="total-results"></p>
-      <p className="total-repos"></p>
-    </nav>
+        <p className='search'>SEARCH: </p>
+        <input id='search-query' placeholder="github repository name"/>
+        <p id="previous-button">previous</p>
+        <p id="next-button">next</p>
+        <p className="search-results"></p>
+        <p className="total-results"></p>
+        <p className="total-repos"></p>
+      </nav>
     );
   }
 }
