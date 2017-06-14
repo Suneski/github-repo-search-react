3a444ac2119180d35872cbc469f98b307af1a339
David Espinoza <despinoza1027@gmail.com>
2017-06-14 13:01:25 -0500

    search results showing

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 
+import SearchResultLi from './SearchResultLi.js'
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       searchTerm: '',
-      page: ''
+      page: '',
+      searchResults: [],
     }
   }
 
   searchQueryText(evt) {
-    console.log(evt.target.value)
     this.setState({
       searchTerm: evt.target.value,
       page: 1
@@ -20,15 +22,31 @@ class App extends Component {
 
   makeAjaxCall() {
     $.ajax({
-      url: 'https://api.github.com/search/repositories?q=' + this.state.searchTerm + '&page=' + '1'
+      url: 'https://api.github.com/search/repositories?q=' + this.state.searchTerm + '&page=' + this.state.page
     })
     .done((data) => {
       console.log(data);
+      this.setState({
+        totalCount: data.total_count,
+        pageCount: Math.ceil(data.total_count / 30),
+        searchResults: data.items,
+      })
     });
   };
 
 
   render() {
+    let results = this.state.searchResults.map((x) => <SearchResultLi
+      key={x.id}
+      login={x.login}
+      html_url={x.html_url}
+      avatar_url={x.owner.avatar_url}
+      repo_name={x.name}
+      username={x.owner.login}
+      repoNameHref={'https://github.com/' + x.full_name}
+      userNameHref={'https://github.com/' + x.owner.login}
+    />);
+
     return (
       <div className="App">
         <nav>
@@ -43,7 +61,7 @@ class App extends Component {
         </nav>
 
         <ul id='repos'>
-
+          {results}
         </ul>
       </div>
     );
