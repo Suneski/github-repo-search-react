a671349d5fe9e23378121ea7f79300ad5afea74f
David Espinoza <despinoza1027@gmail.com>
2017-06-14 00:22:52 -0500

    set up Store

--- react-ui/src/Store.js
+++ react-ui/src/Store.js
@@ -1,1 +1,27 @@
 import { createStore } from 'redux';
+
+const actions = {
+  UPDATE_QUERY_TEXT: 'UPDATE_QUERY_TEXT',
+}
+
+var initialState = {
+  searchTerm: '',
+  page: ''
+};
+
+const reducer = (state = initialState, action) => {
+  console.log('reducer running', action);
+
+
+  switch (action.type) {
+    case actions.UPDATE_QUERY_TEXT:
+      return Object.assign({}, state, {
+        searchTerm: action.value });
+    default: return state
+  }
+}
+
+const store = createStore(reducer);
+
+
+export default store;
